15eb9b156b18661d4f564c82003133a21e950c47
LabbE <leonardolabbe2012@gmail.com>
2024-02-16 17:40:45 -0300

    Revert "Update: created process and processIO structs"
    This reverts commit c2797aaebfc84b040909f756a2bf76d3bb8b1d79.

--- include/Processo/processo.h
+++ include/Processo/processo.h
@@ -1,49 +1,18 @@
+#ifndef PROCESSO_H
+#define PROCESSO_H
 #include "header.h"
-#include "process.h"
-
-
-typedef enum{
-    Disco,
-    Impressora,
-    Fita
-} E_S;
-
-typedef enum{
-    
-    Executando,
-    Pronto,
-    Encerrado,
-    Bloqueado,
-    Não_Vivo
-
-} Estado;
-
-typedef enum{
-    LOW,
-    HIGH
-} Prioridade;
-
 
 typedef struct Processo{
     int tempoCPU;
-    Prioridade prioridade;
-    int pid;
-    int ppid; 
-    Estado estado;
+    int prioridade;
+    int pid; 
+    int estado;
     int tempoChegada;
     int tempoExecucao;
     int tempoTermino;
-    Processo* prox;
-    Processo* ant;
+    Processo *prox
 } Processo;
 
-typedef struct Processo_ES{
-    E_S* tipoES;
-    int* tempoES;
-    int quantES;
-    Prioridade prioridade;
-
-} Processo_ES;
 
 typedef struct Fila{
     Processo *head;
@@ -56,3 +25,5 @@ int FilaVazia(Fila *fila);
 void push(Fila *fila, Processo* processo);
 Processo* pop(Fila *fila);
 void liberarFila(Fila *fila);
+
+#endif
